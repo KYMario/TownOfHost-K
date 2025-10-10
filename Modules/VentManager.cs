d0e5df8afd0d7ef85f74c8358a86d5ea46727f56
BuildTools <unconfigured@null.spigotmc.org>
2025-10-10 14:48:00 +0900

    change: たぶんかるい

--- Modules/VentManager.cs
+++ Modules/VentManager.cs
@@ -6,12 +6,26 @@ using Hazel;
 using TownOfHost.Roles.Core;
 using TownOfHost.Patches.ISystemType;
 using TownOfHost.Roles.Core.Interfaces;
+using TownOfHost.Attributes;
 
 namespace TownOfHost.Modules;
 
 class VentManager
 {
-    public static Dictionary<byte, int> VentDuringDisabling = new();
+    public static Dictionary<byte, int> VentDuringDisabling = null;
+    private static readonly List<byte> _delList = new();
+    private const float VentSqrThreshold = 4; //2^2
+    private static bool MaxInVentMode;
+    private static float MaxInVentTime;
+
+    [GameModuleInitializer]
+    public static void Init()
+    {
+        VentDuringDisabling = new(GameData.Instance.PlayerCount);
+        MaxInVentMode = Options.MaxInVentMode.GetBool();
+        MaxInVentTime = Options.MaxInVentTime.GetFloat();
+    }
+
     public static void UpdateDesyncVentCleaning(PlayerControl player, RoleBase roleclass)
     {
         //ゲーム中でなければ実行しない
@@ -19,61 +33,73 @@ class VentManager
 
         if (Options.CurrentGameMode == CustomGameMode.Standard && GameStates.IsInTask && GameStates.introDestroyed && player.IsAlive() && !player.IsModClient())
         {
-            Dictionary<int, float> Distance = new();
             Vector2 position = player.transform.position;
-            foreach (var vent in ShipStatus.Instance.AllVents)
-                Distance.Add(vent.Id, Vector2.Distance(position, vent.transform.position));
-            var first = Distance.OrderBy(x => x.Value).First();
 
-            if (VentDuringDisabling.TryGetValue(player.PlayerId, out var ventId) && (first.Key != ventId || first.Value > 2))
+            int minId = -1;
+            float minDist = 99;
+            var ventCount = ShipStatus.Instance.AllVents.Count;
+            for (var i = 0; i < ventCount; i++)
             {
-                ushort num = (ushort)(VentilationSystemUpdateSystemPatch.last_opId + 1U);
-                MessageWriter msgWriter = MessageWriter.Get(SendOption.None);
-                msgWriter.Write(num);
-                msgWriter.Write((byte)VentilationSystem.Operation.StopCleaning);
-                msgWriter.Write((byte)ventId);
-                player.RpcDesyncUpdateSystem(SystemTypes.Ventilation, msgWriter);
-                msgWriter.Recycle();
-                VentDuringDisabling.Remove(player.PlayerId);
-                VentilationSystemUpdateSystemPatch.last_opId = num;
+                var vent = ShipStatus.Instance.AllVents[i];
+                var dist = Vector2.SqrMagnitude(position - (Vector2)vent.transform.position);
+                if (minDist < dist) continue;
+                minId = vent.Id;
+                minDist = dist;
             }
-            else if (first.Value <= 2 && !VentDuringDisabling.ContainsKey(player.PlayerId) && (((roleclass as IKiller)?.CanUseImpostorVentButton() is false) || (roleclass?.CanClickUseVentButton == false)))
+
+            if (minId == -1) return;
+
+            if (VentDuringDisabling.TryGetValue(player.PlayerId, out var ventId))
             {
-                ushort num = (ushort)(VentilationSystemUpdateSystemPatch.last_opId + 1U);
-                MessageWriter msgWriter = MessageWriter.Get(SendOption.None);
-                msgWriter.Write(num);
-                msgWriter.Write((byte)VentilationSystem.Operation.StartCleaning);
-                msgWriter.Write((byte)first.Key);
-                player.RpcDesyncUpdateSystem(SystemTypes.Ventilation, msgWriter);
-                msgWriter.Recycle();
-                VentilationSystemUpdateSystemPatch.last_opId = num;
-                VentDuringDisabling[player.PlayerId] = first.Key;
+                if (minId != ventId || minDist > VentSqrThreshold)
+                {
+                    RpcDesyncUpdateVent(player, ventId, VentilationSystem.Operation.StopCleaning);
+                    VentDuringDisabling.Remove(player.PlayerId);
+                }
+            }
+            else if (minDist <= VentSqrThreshold && (((roleclass as IKiller)?.CanUseImpostorVentButton() is false) || (roleclass?.CanClickUseVentButton == false)))
+            {
+                RpcDesyncUpdateVent(player, minId, VentilationSystem.Operation.StartCleaning);
+                VentDuringDisabling[player.PlayerId] = minId;
             }
         }
     }
+
+    private static void RpcDesyncUpdateVent(PlayerControl player, int ventId, VentilationSystem.Operation operation)
+    {
+        ushort num = (ushort)(VentilationSystemUpdateSystemPatch.last_opId + 1U);
+        MessageWriter msgWriter = MessageWriter.Get(SendOption.None);
+        msgWriter.Write(num);
+        msgWriter.Write((byte)operation);
+        msgWriter.Write((byte)ventId);
+        player.RpcDesyncUpdateSystem(SystemTypes.Ventilation, msgWriter);
+        msgWriter.Recycle();
+        VentilationSystemUpdateSystemPatch.last_opId = num;
+    }
+
     public static void CheckVentLimit()
     {
-        if (Options.MaxInVentMode.GetBool())
-        {
-            List<byte> del = new();
-            foreach (var ventpc in CoEnterVentPatch.VentPlayers)
-            {
-                var pc = PlayerCatch.GetPlayerById(ventpc.Key);
-                if (pc == null) continue;
+        if (!MaxInVentMode) return;
 
-                if (ventpc.Value > Options.MaxInVentTime.GetFloat())
+        _delList.Clear();
+
+        foreach (var ventpc in CoEnterVentPatch.VentPlayers)
+        {
+            var pc = PlayerCatch.GetPlayerById(ventpc.Key);
+            if (pc == null) continue;
+
+            if (ventpc.Value > MaxInVentTime)
+            {
+                /*if (!CoEnterVentPatch.VentPlayers.ContainsKey(ventpc.Key))
                 {
-                    if (!CoEnterVentPatch.VentPlayers.TryGetValue(ventpc.Key, out var a))
-                    {
-                        del.Add(ventpc.Key);
-                        continue;
-                    }
-                    pc.MyPhysics.RpcBootFromVent(VentilationSystemUpdateSystemPatch.NowVentId.TryGetValue(ventpc.Key, out var ventid) ? ventid : 0);
                     del.Add(ventpc.Key);
-                }
-                CoEnterVentPatch.VentPlayers[ventpc.Key] += Time.fixedDeltaTime;
+                    continue;
+                }*/
+                pc.MyPhysics.RpcBootFromVent(VentilationSystemUpdateSystemPatch.NowVentId.TryGetValue(ventpc.Key, out var ventid) ? ventid : 0);
+                _delList.Add(ventpc.Key);
             }
-            del.Do(id => CoEnterVentPatch.VentPlayers.Remove(id));
+            CoEnterVentPatch.VentPlayers[ventpc.Key] += Time.fixedDeltaTime;
         }
+        _delList.Do(id => CoEnterVentPatch.VentPlayers.Remove(id));
     }
 }
