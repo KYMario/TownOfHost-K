27f657686735dc7e33dd1ba732f8f00467b3e0cb
Yoran_Furan <yon.yoran.f.2nd@gmail.com>
2025-10-13 19:36:02 +0900

    fix : システムメッセージの時は名前色の処理をしない

--- Patches/ChatBubblePatch.cs
+++ Patches/ChatBubblePatch.cs
@@ -13,7 +13,7 @@ namespace TownOfHost.Patches
             if (GameStates.IsInGame)
             {
                 if (!__instance.playerInfo._object) return;
-                if (__instance.TextArea.text != string.Empty) //投票通知ではないなら
+                if (__instance.TextArea.text != string.Empty && IsSystemMeg is false) //投票通知ではないなら
                 {
                     __instance.NameText.text = __instance.playerInfo.GetLogPlayerName().ApplyNameColorData(PlayerControl.LocalPlayer, __instance.playerInfo._object, true);
                     return;
