e0dc1d80a2a2cf7498978f0aad71e217585e6e9f
Yoran_Furan <yon.yoran.f.2nd@gmail.com>
2026-03-09 20:51:06 +0900

    add : Wolfboyに一定数キルしないと勝利できない設定の追加

--- Roles/Crewmate/WolfBoy.cs
+++ Roles/Crewmate/WolfBoy.cs
@@ -36,6 +36,13 @@ public sealed class WolfBoy : RoleBase, IKiller, ISchrodingerCatOwner
         ShotLimit = ShotLimitOpt.GetInt();
         CurrentKillCooldown = KillCooldown.GetFloat();
         HasImpV = ImpostorVision.GetBool();
+
+        CanwinKillcount = optcanwinkillcount.GetInt();
+        IsCountCrew = optcountcrew.GetBool();
+        IsCountMadmate = optcountmadmate.GetBool();
+        IsCountImpostor = optcountimpstor.GetBool();
+        IsCountNeutral = optcountneutral.GetBool();
+        NowKillcount = 0;
     }
 
     public static OptionItem KillCooldown;
@@ -43,11 +50,19 @@ public sealed class WolfBoy : RoleBase, IKiller, ISchrodingerCatOwner
     public static OptionItem CanKillAllAlive;
     public static OptionItem Shurenekodotti;
     public static OptionItem ImpostorVision;
+    public static OptionItem optcanwinkillcount; static int CanwinKillcount;
+    public static OptionItem optcountcrew; static bool IsCountCrew;
+    public static OptionItem optcountimpstor; static bool IsCountImpostor;
+    public static OptionItem optcountmadmate; static bool IsCountMadmate;
+    public static OptionItem optcountneutral; static bool IsCountNeutral;
+    int NowKillcount;
     enum OptionName
     {
         SheriffShotLimit,
         SheriffCanKillAllAlive,
-        WolfBoySchrodingerCatTime
+        WolfBoySchrodingerCatTime,
+        Wolfboycanwinkillcount,
+        WolfBoyCountteam
     }
     public int ShotLimit = 0;
     public float CurrentKillCooldown = 30;
@@ -64,11 +79,21 @@ public sealed class WolfBoy : RoleBase, IKiller, ISchrodingerCatOwner
         KillCooldown = FloatOptionItem.Create(RoleInfo, 10, GeneralOption.KillCooldown, new(0f, 180f, 0.5f), 30f, false)
             .SetValueFormat(OptionFormat.Seconds);
         OverrideKilldistance.Create(RoleInfo, 8);
+        optcanwinkillcount = IntegerOptionItem.Create(RoleInfo, 15, OptionName.Wolfboycanwinkillcount, new(0, 14, 1), 2, false).SetZeroNotation(OptionZeroNotation.Off).SetValueFormat(OptionFormat.Players);
+        optcountcrew = BooleanOptionItem.Create(RoleInfo, 16, OptionName.WolfBoyCountteam, true, false, optcanwinkillcount);
+        optcountimpstor = BooleanOptionItem.Create(RoleInfo, 17, OptionName.WolfBoyCountteam, false, false, optcanwinkillcount);
+        optcountmadmate = BooleanOptionItem.Create(RoleInfo, 18, OptionName.WolfBoyCountteam, false, false, optcanwinkillcount);
+        optcountneutral = BooleanOptionItem.Create(RoleInfo, 19, OptionName.WolfBoyCountteam, false, false, optcanwinkillcount);
         ShotLimitOpt = IntegerOptionItem.Create(RoleInfo, 11, OptionName.SheriffShotLimit, new(1, 15, 1), 15, false)
             .SetValueFormat(OptionFormat.Times);
         CanKillAllAlive = BooleanOptionItem.Create(RoleInfo, 12, OptionName.SheriffCanKillAllAlive, true, false);
         ImpostorVision = BooleanOptionItem.Create(RoleInfo, 13, GeneralOption.ImpostorVision, true, false);
         Shurenekodotti = BooleanOptionItem.Create(RoleInfo, 14, OptionName.WolfBoySchrodingerCatTime, false, false);
+
+        optcountcrew.ReplacementDictionary = new() { { "%role%", UtilsRoleText.GetRoleColorAndtext(CustomRoles.Crewmate) } };
+        optcountimpstor.ReplacementDictionary = new() { { "%role%", UtilsRoleText.GetRoleColorAndtext(CustomRoles.Impostor) } };
+        optcountmadmate.ReplacementDictionary = new() { { "%role%", UtilsRoleText.GetRoleColorAndtext(CustomRoles.Madmate) } };
+        optcountneutral.ReplacementDictionary = new() { { "%role%", $"<#cccccc>{GetString("Neutral")}</color>" } };
     }
     public override void Add()
     {
@@ -130,11 +155,33 @@ public sealed class WolfBoy : RoleBase, IKiller, ISchrodingerCatOwner
                 info.DoKill = false;
                 return;
             }
-            if (target.Is(CustomRoleTypes.Impostor)) Achievements.RpcCompleteAchievement(Player.PlayerId, 0, achievements[0]);
-
             killer.ResetKillCooldown();
         }
         return;
+    }
+    void IKiller.OnMurderPlayerAsKiller(MurderInfo info)
+    {
+        var target = info.AttemptTarget;
+        if (target.Is(CustomRoleTypes.Impostor)) Achievements.RpcCompleteAchievement(Player.PlayerId, 0, achievements[0]);
+        switch (target.GetCustomRole().GetCustomRoleTypes())
+        {
+            case CustomRoleTypes.Crewmate:
+                if (IsCountCrew)
+                    NowKillcount++;
+                break;
+            case CustomRoleTypes.Madmate:
+                if (IsCountMadmate)
+                    NowKillcount++;
+                break;
+            case CustomRoleTypes.Impostor:
+                if (IsCountImpostor)
+                    NowKillcount++;
+                break;
+            case CustomRoleTypes.Neutral:
+                if (IsCountNeutral)
+                    NowKillcount++;
+                break;
+        }
     }
     public override string GetProgressText(bool comms = false, bool gamelog = false) => Utils.ColorString(CanUseKillButton() ? Color.yellow : Color.gray, $"({ShotLimit})");
     public static bool CanBeKilledBy(PlayerControl player)
@@ -155,6 +202,30 @@ public sealed class WolfBoy : RoleBase, IKiller, ISchrodingerCatOwner
             CustomRoleTypes.Impostor => cRole is not CustomRoles.Tairou,
             _ => true,
         };
+    }
+    public override void CheckWinner(GameOverReason reason)
+    {
+        if (CustomWinnerHolder.winners.Contains(CustomWinner.Crewmate) && Player.IsLovers() is false &&
+        Player.Is(CustomRoles.Amanojaku) is false)
+        {
+            // 勝利に必要なキル数に届いていない場合
+            if (CanwinKillcount > NowKillcount)
+            {
+                CustomWinnerHolder.CantWinPlayerIds.Add(Player.PlayerId);
+                Logger.Info($"狼少年はキルしてないから負け！", "Wolfboy");
+            }
+        }
+    }
+    public override string GetLowerText(PlayerControl seer, PlayerControl seen = null, bool isForMeeting = false, bool isForHud = false)
+    {
+        if (CanwinKillcount is 0) return "";
+
+        var teamtext = "";
+        if (IsCountImpostor) teamtext += $"<#ff1919>{GetString("Impostor")}</color>";
+        if (IsCountMadmate) teamtext += $"<#ff7f50>{GetString("Madmate")}</color>";
+        if (IsCountCrew) teamtext += $"<#8cffff>{GetString("Crewmate")}</color>";
+        if (IsCountNeutral) teamtext += $"<#cccccc>{GetString("Neutral")}</color>";
+        return string.Format(GetString("WolfBoy_LowerText"), CanwinKillcount, teamtext);
     }
     public bool OverrideKillButton(out string text)
     {
